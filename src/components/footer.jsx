312ed9745bf9ef96dde253bd1091adaedba7e8a5
Manoj kumar <manojamarapu@gmail.com>
2026-04-13 20:31:39 +0530

    chore: remove unused eslint configuration and synchronize user deleted files

--- src/components/footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-black text-center py-4 text-gray-500">
-      © 2026 A. M. Manoj Kumar. All rights reserved.
-    </footer>
-  );
-}
